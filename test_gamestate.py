85e6c020862c97dd1cb822a2fbfe50f136d9821d
Zach Lee <zach3lee@gmail.com>
2017-08-16 23:53:48 -0700

    takes set commands now instead of arbitrary code

--- test_gamestate.py
+++ test_gamestate.py
@@ -6,8 +6,22 @@ game_state = gamestate.GameState()
 
 input_string = None
 while input_string != 'q':
+	if input_string == 'left':
+		game_state.turn_left()
+	elif input_string == 'right':
+		game_state.turn_right()
+	elif input_string == 'forward':
+		game_state.move_forward()
+	elif input_string == 'shoot':
+		game_state.shoot()
+	elif input_string == 'grab':
+		game_state.grab()
+	elif input_string == 'climb':
+		game_state.climb()
+	else:
+		print('command not recognized: {}'.format(input_string))
 	game_state._cave.print()
 	print('player: {}'.format(game_state.player_location))
 	print('facing: {}'.format(game_state.player_orientation))
 	print(game_state.get_percepts())
-	print(exec(input('next move? ')))
+	input_string = input('next move? ')
