dd1d250c63ad4bf9525d04bd97727effe930d74a
Zach Lee <zach3lee@gmail.com>
2017-09-26 12:30:48 -0700

    Add generator to generate each iteration of an AI turn

--- agent.py
+++ agent.py
@@ -1,3 +1,5 @@
+import gamestate
+
 class Agent(object):
     possible_moves = ['left', 'right', 'forward', 'shoot', 'grab', 'climb']
     def __init__(self):
@@ -10,6 +12,32 @@ class Agent(object):
         pass
 
 
+class AgentException(Exception):
+    pass
+
+
 def poll_agent(agent, percepts):
     agent.handle_percepts(percepts)
     return agent.get_move()
+
+
+def get_ai_iteration_generator(agent):
+    current_state = gamestate.GameState()
+    while True: # no end condition defined by book
+        current_percepts = current_state.get_percepts()
+        agent_move = poll_agent(agent, current_percepts)
+        if agent_move == 'left':
+            current_state.turn_left()
+        elif agent_move == 'right':
+            current_state.turn_right()
+        elif agent_move == 'forward':
+            current_state.move_forward()
+        elif agent_move == 'shoot':
+            current_state.shoot()
+        elif agent_move == 'grab':
+            current_state.grab()
+        elif agent_move == 'climb':
+            current_state.climb()
+        else:
+            raise AgentException('Agent returned an illegal action: {}'.format(agent_move))
+        yield current_state
